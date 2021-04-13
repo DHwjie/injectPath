5b0e8996c85855f0cec367969393958520c0199d
DHwjie <27079763@qq.com>
2021-04-13 20:54:07 +0800

    1

---
+++ inject_path.py
@@ -0,0 +1,9 @@
+from os import *
+
+truck_file_list = listdir('truck')
+
+print(len(truck_file_list))
+print(truck_file_list)
+total_num_truckfile = len(truck_file_list)
+
+print(type(truck_file_list))
